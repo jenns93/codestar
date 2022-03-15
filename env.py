17efa7e5cf77da44aa8096ec57fbc5de19d66435
jenns93 <jjenns93@gmail.com>
2022-03-15 13:42:52 +0000

    Deployment Commit

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+os.environ["DATABASE_URL"]="postgres://paznwhtqwprrqg:70bdbc5ee2cff3436133066028b8d57d75d3fbfdb7202c511f0bef218c399173@ec2-54-73-152-36.eu-west-1.compute.amazonaws.com:5432/d1dmlhdnhe5jdn"
+os.environ["SECRET_KEY"]="codestarkey"
+os.environ["CLOUDINARY_URL"]="cloudinary://271659233865527:bFO42VjRSAD5Rc2J3Utv6NtHpgo@dfa0augxk"
